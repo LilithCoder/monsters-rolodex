1a3447ad4dabf748de5e6a2eed7bc4a0d54a370b
Weilin Gao <william.wlgao@gmail.com>
2019-07-17 18:18:50 +0800

    Updates

---
+++ precache-manifest.65e73f144b1c6deda6d344fc772406a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e272ced60ce2e822093cb3ddf5b04f9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0d9702fe7b62f8d15c06",
+    "url": "/monsters-rolodex/static/css/main.edeae695.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0d9702fe7b62f8d15c06",
+    "url": "/monsters-rolodex/static/js/main.54eca7cf.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
